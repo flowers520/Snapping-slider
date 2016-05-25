42e337e61e76dd1ec9cde9409ebab854e7e8c098
flowers520 <m18773359628>
2016-05-25 09:17:38 +0800

    Initial Commit

---
+++ Snapping slider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapping slider
+//
+//  Created by jim on 16/5/25.
+//  Copyright © 2016年 jim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
